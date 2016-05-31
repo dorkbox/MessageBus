f8747c19219d524857c88503d6bd99d39682e4e0
nathan <nathan@dorkbox.com>
2016-05-31 02:21:42 +0200

    Updated version

--- src/dorkbox/messagebus/MessageBus.java
+++ src/dorkbox/messagebus/MessageBus.java
@@ -107,7 +107,7 @@ class MessageBus implements IMessageBus {
      */
     public static
     String getVersion() {
-        return "1.14";
+        return "1.15";
     }
 
     private final ErrorHandler errorHandler;
